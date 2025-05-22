a406755d971bed76e02c2a3697905281ac022f53
JhanPier090 <jhanmontes090@gmail.com>
2025-05-21 22:05:52 -0300

    Add files via upload

---
+++ PruebaVariables.java
@@ -0,0 +1,41 @@
+package Jhan;
+public class PruebaVariables {
+    
+    public static void main(String[] args){
+    System.out.println("1)");
+    int edad = 17;
+    System.out.println("La edad es: " + edad);
+     System.out.println("2)");
+    double precioproducto = 45.99;
+    System.out.println("El precio del producto es: " + precioproducto);
+    System.out.println("3)");
+    boolean tieneDescuento = true;
+    System.out.println("¿Tiene descuento? " + tieneDescuento);
+    System.out.println("4)");
+    char letra = 'J';
+    System.out.println("La letra es: " + letra);
+    System.out.println("5)");
+    int puntos = 10; 
+    System.out.println("Puntos: " + puntos);
+     puntos = 20;
+    System.out.println("Puntos: " + puntos);
+    System.out.println("6)");
+    double notaFinal = 8;
+    System.out.println(  notaFinal >=6 ? "Aprobado" : "Reprobado"); 
+    System.out.println("7)");
+    double temperatura ;
+    temperatura = 36.5;
+    System.out.println("La temperatura es: " + temperatura);
+    System.out.println("8)");
+    int id = 12; 
+    System.out.println("Si yo pongo un numero en el nombre de la variable no se ejecuta y salta error ");
+    System.out.println("9)");
+    int edad1 = 17;
+    boolean Mayoredad = true;
+    System.out.println(Mayoredad ? edad1 >= 18 ? "Es mayor de edad" : "Es menor de edad" : "No es mayor de edad"); 
+    System.out.println("10)");
+    int x = 5; int y = 10; int z = 15;
+    System.out.println( "valor x " + x + " valor y " + y + " valor z " + z);
+     
+    }
+}
